5bc9cee93b639d8b7a7d4a28af4e43f0ed847a80
Dev DML <dmartinez@esgasoluciones.mx>
2024-09-19 13:10:54 -0600

    Added examples of states [Task #8]

--- src/components/todo/TodoSearch.jsx
+++ src/components/todo/TodoSearch.jsx
@@ -1,9 +1,18 @@
+import React from 'react';
+
 function TodoSearch() 
 {
+    const [searchValue, setSearchValue] = React.useState(''); // Create a state to store the search value, and a function to update it | The initial value is an empty string
+
+    console.log("El usuario busco: " + searchValue); // Log the search value | This will be executed every time the component is rendered | Thc console log can be shown twice because the strict mode of React is enabled by default
+
     return(
-        <input onChange={(event) => {
-            console.log("Se busco la palabra: " + event.target.value);            
-        }} id="search" name="search" className="input" placeholder="Search task" />
+        <input 
+            value={searchValue}  // Set the value of the input
+            onChange={(event) => { // Add an event listener to the input
+            //console.log("Se busco la palabra: " + event.target.value); // Log the search value  
+            setSearchValue(event.target.value); // Update the search value          
+        }} id="search" name="search" className="input" placeholder="Search task" /> 
     );
 }
 
