4d1d81668a2523d52737b9de3b8dc9132ea435c8
Dev DML <dmartinez@esgasoluciones.mx>
2024-09-20 11:35:15 -0600

    New project structure #2 [Task #19]

---
+++ src/App/AppUI.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable react/prop-types */
+import { TodoCounter } from '../components/todo/TodoCounter'
+import { TodoSearch } from '../components/todo/TodoSearch'
+import { TodoList } from '../components/todo/TodoList'
+import { TodoItem } from '../components/todo/TodoItem'
+import { CreateTodoButton } from '../components/todo/CreateTodoButton'
+import { CategoryList } from '../components/todo/CategoryList'
+import { CategoryItem } from '../components/todo/CategoryItem'
+
+import './App.css'
+import '../assets/css/todo-default.css'
+
+function AppUI({
+    categories, 
+    selectedCategory, 
+    selectedAll, 
+    searchValue, 
+    setSearchValue, 
+    searchedTodos, 
+    todoCompleted, 
+    todoDeleted, 
+    completedTodos, 
+    totalTodos
+}) {
+  return (
+    <>      
+      <div className='flex'>
+        <div className='card'>
+          <h2 className='text-center'>
+            Categorías
+          </h2>
+          <CategoryList>
+            <li className="categoryItem" onClick={selectedAll}>
+              Todas
+            </li>
+            {categories.map(category => (
+              <CategoryItem key={category.id} text={category.name} selectedCategory={() => selectedCategory(category.id)} />
+            ))}
+          </CategoryList>         
+          <button className="btn">
+            Crear nueva categoría
+          </button>
+        </div>
+        <div className='flex-1'>
+          <div className='flex align-center'>
+            <h1>
+              Mis tareas
+            </h1>
+            <CreateTodoButton />
+          </div>
+          <div className='flex align-center'>
+            <div>
+              <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} /> {/* Pass the props to the component */}                      
+            </div>
+            <div>
+              <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
+          </div>
+
+          <TodoList>
+            {searchedTodos.map(todo => (
+              <TodoItem 
+                key={todo.id} 
+                text={todo.text} 
+                completed={todo.completed} 
+                onCompleted={() => todoCompleted(todo.id)} 
+                onDeleted={() => todoDeleted(todo.id)}                
+              />
+            ))}
+          </TodoList>          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export { AppUI }; 
--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
-import { TodoCounter } from '../components/todo/TodoCounter'
-import { TodoSearch } from '../components/todo/TodoSearch'
-import { TodoList } from '../components/todo/TodoList'
-import { TodoItem } from '../components/todo/TodoItem'
-import { CreateTodoButton } from '../components/todo/CreateTodoButton'
-import { CategoryList } from '../components/todo/CategoryList'
-import { CategoryItem } from '../components/todo/CategoryItem'
-import { useLocalStorage } from './Hooks/useLocalStorage'
-import './App.css'
-import '../assets/css/todo-default.css'
+import { useLocalStorage } from '../Hooks/useLocalStorage'
+import { AppUI } from './AppUI'
 
 const categories = [
   { id: 1, name: 'Home' },
@@ -77,54 +69,18 @@ function App() {
   };
 
   return (
-    <>      
-      <div className='flex'>
-        <div className='card'>
-          <h2 className='text-center'>
-            Categorías
-          </h2>
-          <CategoryList>
-            <li className="categoryItem" onClick={selectedAll}>
-              Todas
-            </li>
-            {categories.map(category => (
-              <CategoryItem key={category.id} text={category.name} selectedCategory={() => selectedCategory(category.id)} />
-            ))}
-          </CategoryList>         
-          <button className="btn">
-            Crear nueva categoría
-          </button>
-        </div>
-        <div className='flex-1'>
-          <div className='flex align-center'>
-            <h1>
-              Mis tareas
-            </h1>
-            <CreateTodoButton />
-          </div>
-          <div className='flex align-center'>
-            <div>
-              <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} /> {/* Pass the props to the component */}                      
-            </div>
-            <div>
-              <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-            </div>
-          </div>
-
-          <TodoList>
-            {searchedTodos.map(todo => (
-              <TodoItem 
-                key={todo.id} 
-                text={todo.text} 
-                completed={todo.completed} 
-                onCompleted={() => todoCompleted(todo.id)} 
-                onDeleted={() => todoDeleted(todo.id)}                
-              />
-            ))}
-          </TodoList>          
-        </div>
-      </div>
-    </>
+    <AppUI 
+    categories={ categories }
+    selectedCategory={ selectedCategory }
+    selectedAll={ selectedAll}
+    searchValue={ searchValue }
+    setSearchValue={ setSearchValue }
+    searchedTodos={ searchedTodos }
+    todoCompleted={ todoCompleted }
+    todoDeleted={ todoDeleted }
+    completedTodos={ completedTodos }
+    totalTodos={ totalTodos }
+    />
   )
 }
 
